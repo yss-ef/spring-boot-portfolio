bbe9eb536f76c3549e77eb41753227ba9ab29eef
unknown <fellahyoussef010@gmail.com>
2025-11-26 09:27:35 +0100

    ajout du README

--- 04-web-mvc/mvc-security-jpa-lab/src/main/java/com/youssef/springweb/security/SecurityConfig.java
+++ 04-web-mvc/mvc-security-jpa-lab/src/main/java/com/youssef/springweb/security/SecurityConfig.java
@@ -37,11 +37,13 @@ public class SecurityConfig {
         return http
                 //.formLogin(Customizer.withDefaults())
                 .formLogin(fl->fl.loginPage("/login").defaultSuccessUrl("/user/products",true).permitAll())
-                // L'ordre ici est important.
-                .authorizeHttpRequests(ar->ar.requestMatchers("/webjars/** ","/public/**").permitAll()) // ressources publique
-                .authorizeHttpRequests(ar->ar.requestMatchers("/user/**").hasRole("USER")) // Les utilisateurs peuvent visionner la liste des produits
-                .authorizeHttpRequests(ar->ar.requestMatchers("/admin/**").hasRole("ADMIN")) // Les administrateurs peuvent
-                .authorizeHttpRequests(ar->ar.anyRequest().authenticated()) // Appliquer par default
+                // L'ordre des règles d'autorisation est important.
+                .authorizeHttpRequests(ar -> ar
+                        .requestMatchers("/webjars/**", "/public/**").permitAll() // ressources publiques
+                        .requestMatchers("/user/**").hasRole("USER") // Nécessite le rôle USER
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // Nécessite le rôle ADMIN
+                        .anyRequest().authenticated() // Toutes les autres requêtes nécessitent une authentification
+                )
                 .exceptionHandling(eh->eh.accessDeniedPage("/notAuthorized"))
                 .build();
     }
